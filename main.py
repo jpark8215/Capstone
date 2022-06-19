712d1322d57bc60b009fc0652483220578b430f6
jpark8215 <50881191+jpark8215@users.noreply.github.com>
2022-06-18 21:22:03 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-
 import sys
-import project
 import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.metrics import accuracy_score
+import project
 
 while True:
 
     print("1> View graphics")
-    print("2> Prediction")
+    print("2> Training set performance")
+    print("3> Prediction on test set")
     print("0> Exit Application\n")
 
     selected = input("Please enter an option above: ")
@@ -14,23 +16,75 @@ while True:
     if selected == '0':
         sys.exit()
 
-    #
+    # option 1
     if selected == '1':
-
-        print("Hello. Welcome to vaccine predictor! \n "
-              "The following three charts show vaccine status of population we gathered data from. \n"
-              "The first chart will show you the proportion of vaccine status based on the total population. \n"
+        print("Hello. Welcome to vaccine predictor! \n"
+              "* The following three charts show vaccine status of population we gathered data from. \n"
+              "* The first chart will show you the proportion of vaccine status based on the total population. \n"
               "0 means no vaccine received and 1 means vaccine received. \n"
-              "The second chart will show you the H1N1 and seasonal flu vaccine status based on their concerns. \n"
+              "* The second chart will show you the H1N1 vaccine status based on the concerns. \n"
               "0 means no vaccine received or has no concerns. And 1 means vaccine received or has concerns. \n"
-              "The third chart will show the H1N1 and seasonal flu vaccine status based on opinion on vaccine. \n"
-              "0 means no vaccine received or negative opinion. And 1 means vaccine received or positive opinion")
+              "* The third chart will show the H1N1 and seasonal flu vaccine status based on opinion on vaccine. \n"
+              "0 means no vaccine received or negative opinion. And 1 means vaccine received or positive opinion.")
+
+        # first chart
         project.vac_rate()
+        # second chart
         project.vac_rate_concern()
-        # project.vac_rate_op(col, target, df_all, ax=None)
+        # third chart
+        cols_to_plot = [
+            'opinion_h1n1_vacc_effective',
+            'opinion_h1n1_risk',
+            'opinion_seas_vacc_effective',
+            'opinion_seas_risk',
+        ]
+
+        fig, ax = plt.subplots(len(cols_to_plot), 2, figsize=(10, len(cols_to_plot) * 3))
+        for idx, col in enumerate(cols_to_plot):
+            project.vac_rate_op(col, 'h1n1_vaccine', project.df_all, ax=ax[idx, 0])
+            project.vac_rate_op(col, 'seasonal_vaccine', project.df_all, ax=ax[idx, 1])
+
+        ax[0, 0].legend(loc='best', title='vaccine status')
+        # ax[0, 0].legend(loc='best', title='H1N1 vaccine')
+        # ax[0, 1].legend(loc='best', title='Seasonal vaccine')
+        fig.tight_layout()
         plt.show()
 
-    #
+    # option 2
     if selected == '2':
-        # project.plot_roc()
+        print("*The chart will show you AUC-ROC curve for H1N1 and seasonal flu vaccine prediction on training set. \n"
+              "AUC - ROC curve is a classification problem performance measurement. \n"
+              "AOC is the area under the ROC curve, which is probability \n"
+              "that a randomly chosen positive case is ranked higher than a randomly chosen negative case. \n"
+              "ROC is calculated by the true positive rate (TPR) against the false positive rate (FPR).\n"
+              "*The accuracy score on training set is as shown below. \n")
+        # accuracy score of training set
+        project.full_pipeline.fit(project.df1, project.df2)
+
+        org_data = project.df2
+        eval_data = project.full_pipeline.predict_proba(project.df1)
+
+        predict_data = pd.DataFrame({"h1n1_vaccine": eval_data[0][:, 1], "seasonal_vaccine": eval_data[1][:, 1]},
+                                    index=project.df2.index)
+        predict_data["h1n1_vaccine"] = predict_data["h1n1_vaccine"].round(0)
+        predict_data["seasonal_vaccine"] = predict_data["seasonal_vaccine"].round(0)
+
+        print("Accuracy Score:", (accuracy_score(org_data, predict_data) * 100).__round__(2), "%\n")
+
+        # auc-roc curve chart for training set
+        fig, ax = plt.subplots(1, 2, figsize=(7, 3.5))
+
+        project.plot_roc(project.y_eval['h1n1_vaccine'], project.eval_predict_set['h1n1_vaccine'],
+                         'h1n1_vaccine', ax=ax[0])
+        project.plot_roc(project.y_eval['seasonal_vaccine'], project.eval_predict_set['seasonal_vaccine'],
+                         'seasonal_vaccine', ax=ax[1])
+        fig.tight_layout()
+        plt.show()
+
+    # option 3
+    if selected == '3':
+        # test set prediction
         print(project.test_predict_set)
+
+        while True:
+            selected = input("Please enter the participant's id you would like to look up (26707 - 53414): ")
--- project.py
+++ project.py
@@ -10,44 +10,45 @@ from sklearn.multioutput import MultiOutputClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
-col_names = ["respondent_id",
-             "h1n1_concern",
-             "h1n1_knowledge",
-             "behavioral_antiviral_meds",
-             "behavioral_avoidance",
-             "behavioral_face_mask",
-             "behavioral_wash_hands",
-             "behavioral_large_gatherings",
-             "behavioral_outside_home",
-             "behavioral_touch_face",
-             "doctor_recc_h1n1",
-             "doctor_recc_seasonal",
-             "chronic_med_condition",
-             "child_under_6_months",
-             "health_worker",
-             "health_insurance",
-             "opinion_h1n1_vacc_effective",
-             "opinion_h1n1_risk",
-             "opinion_h1n1_sick_from_vacc",
-             "opinion_seas_vacc_effective",
-             "opinion_seas_risk",
-             "opinion_seas_sick_from_vacc",
-             "age_group",
-             "education",
-             "race",
-             "sex",
-             "income_poverty",
-             "marital_status",
-             "rent_or_own",
-             "employment_status",
-             "hhs_geo_region",
-             "census_msa",
-             "household_adults",
-             "household_children",
-             "employment_industry",
-             "employment_occupation",
-             "h1n1_vaccine",
-             "seasonal_vaccine"]
+# columns in features
+cols = ["respondent_id",
+        "h1n1_concern",
+        "h1n1_knowledge",
+        "behavioral_antiviral_meds",
+        "behavioral_avoidance",
+        "behavioral_face_mask",
+        "behavioral_wash_hands",
+        "behavioral_large_gatherings",
+        "behavioral_outside_home",
+        "behavioral_touch_face",
+        "doctor_recc_h1n1",
+        "doctor_recc_seasonal",
+        "chronic_med_condition",
+        "child_under_6_months",
+        "health_worker",
+        "health_insurance",
+        "opinion_h1n1_vacc_effective",
+        "opinion_h1n1_risk",
+        "opinion_h1n1_sick_from_vacc",
+        "opinion_seas_vacc_effective",
+        "opinion_seas_risk",
+        "opinion_seas_sick_from_vacc",
+        "age_group",
+        "education",
+        "race",
+        "sex",
+        "income_poverty",
+        "marital_status",
+        "rent_or_own",
+        "employment_status",
+        "hhs_geo_region",
+        "census_msa",
+        "household_adults",
+        "household_children",
+        "employment_industry",
+        "employment_occupation",
+        "h1n1_vaccine",
+        "seasonal_vaccine"]
 
 # load dataset from training set files
 df1 = pd.read_csv('training_set_features.csv', index_col="respondent_id")
@@ -57,9 +58,9 @@ df2 = pd.read_csv('training_set_labels.csv', index_col="respondent_id")
 df_all = df1.merge(df2, how="left", on="respondent_id")
 
 
+# distribution of the two target variables
+# display in horizontal graphs
 def vac_rate():
-    # distribution of the two target variables
-    # display in plt graph
     fig, ax = plt.subplots(2, 1)
     n_obs = df_all.shape[0]
 
@@ -110,42 +111,31 @@ feature_cols = ["h1n1_concern",
                 ]
 
 
+# vaccine rate base on disease concerns
+# display in horizontal graphs
 def vac_rate_concern():
     # count of observations for each combination of two variables
-    counts = (
-        df_all[['h1n1_concern', 'h1n1_vaccine']].groupby(['h1n1_concern', 'h1n1_vaccine']).size().unstack(
-            'h1n1_vaccine'))
-    # ax = counts.plot.barh()
-    # ax.invert_yaxis()
-    # ax.set_xlabel('number of h1n1_vaccine')
-    # ax.legend(loc='best', title='h1n1_vaccine')
-    # plt.show()
+    h1n1_counts = (df_all[['h1n1_concern', 'h1n1_vaccine']].groupby(['h1n1_concern', 'h1n1_vaccine']).size().unstack(
+        'h1n1_vaccine'))
 
     # rate of vaccination for each level of h1n1_concern
-    # stacked
-    h1n1_concern_counts = counts.sum(axis='columns')
-    props = counts.div(h1n1_concern_counts, axis='index')
-    # ax = props.plot.barh()
-    # ax.invert_yaxis()
-    # ax.set_xlabel('rate of h1n1_vaccine')
-    # ax.legend(loc='best', title='h1n1_vaccine')
-    # plt.show()
+    h1n1_concern_counts = h1n1_counts.sum(axis='columns')
+    props = h1n1_counts.div(h1n1_concern_counts, axis='index')
 
     # rate of vaccination for each level of h1n1_concern
-    # side by side
-    ax = props.plot.barh(stacked=True)
+    ax = props.plot.barh(title="Rate of H1N1 Vaccine")
     ax.invert_yaxis()
-    ax.set_xlabel('rate of h1n1_vaccine')
-    ax.legend(loc='best', title='h1n1_vaccine')
+    ax.set_xlabel('H1N1 vaccine rate')
+    ax.legend(loc='best', title='H1N1 vaccine')
     plt.show()
 
 
+# vaccine rate base on opinions
+# display in horizontal graphs
 def vac_rate_op(col, target, df_all, ax=None):
-    # Stacked bar chart of vaccination rate for `target` against`col`.
-    # col (string): column name of feature variable
-    # target (string): column name of target variable
-    # data (pandas DataFrame): dataframe that contains columns `col` and `target`
-    # ax (matplotlib axes object, optional): matplotlib axes object to attach plot to
+    # col: column name of feature variable
+    # target: column name of target variable
+    # df: dataframe that contains columns `col` and `target`
 
     feature_counts = (df_all[[target, col]].groupby([target, col]).size().unstack(target))
     group_counts = feature_counts.sum(axis='columns')
@@ -156,30 +146,11 @@ def vac_rate_op(col, target, df_all, ax=None):
     ax.legend().remove()
 
 
-cols_to_plot = [
-    'opinion_h1n1_vacc_effective',
-    'opinion_h1n1_risk',
-    'opinion_seas_vacc_effective',
-    'opinion_seas_risk',
-]
-
-fig, ax = plt.subplots(len(cols_to_plot), 2, figsize=(10, len(cols_to_plot) * 3))
-for idx, col in enumerate(cols_to_plot):
-    vac_rate_op(col, 'h1n1_vaccine', df_all, ax=ax[idx, 0])
-    vac_rate_op(col, 'seasonal_vaccine', df_all, ax=ax[idx, 1])
-
-ax[0, 0].legend(loc='best', title='h1n1_vaccine')
-ax[0, 1].legend(loc='best', title='seasonal_vaccine')
-fig.tight_layout()
-# plt.show()
-
 # train model using logistic regression
 # set a random seed for reproducibility
-RANDOM_SEED = 5
+RANDOM_SEED = 15
 
-# non-numeric value processing
-# process object values
-# replacing values
+# replace non-numeric object values into numeric values
 df1['age_group'].replace(['18 - 34 Years', '35 - 44 Years', '45 - 54 Years', '55 - 64 Years', '65+ Years'],
                          [0, 1, 2, 3, 4], inplace=True)
 df1['education'].replace(['< 12 Years', '12 Years', 'Some College', 'College Graduate'], [0, 1, 2, 3], inplace=True)
@@ -194,15 +165,12 @@ df1['census_msa'].replace(['Non-MSA', 'MSA, Not Principle  City', 'MSA, Principl
 # process numeric values
 numeric_cols = df1.columns[df1.dtypes != "object"].values
 
-# chain preprocessing into a Pipeline object
-# each step is a tuple of (name you chose, sklearn transformer)
-# Z-score scaling. This scales and shifts features so that they have zero mean and unit variance
-# NA median imputation, which fills missing values with the median from the training data
+# chain preprocessing into a pipeline object into a tuple of (numeric value, sklearn transformer)
+# Z-score scaling scales and shifts features so that they have zero mean and unit variance
+# NA mean imputation fills missing values with the mean value of the training set
 numeric_preprocessing_steps = Pipeline(
-    [('standard_scaler', StandardScaler()), ('simple_imputer', SimpleImputer(strategy='median'))])
+    [('standard_scaler', StandardScaler()), ('simple_imputer', SimpleImputer(strategy='mean'))])
 
-# create the preprocessor stage of final pipeline
-# each entry in the transformer list is a tuple of (name you choose, sklearn transformer, list of columns)
 preprocessor = ColumnTransformer(transformers=
                                  [("numeric", numeric_preprocessing_steps, numeric_cols)], remainder="drop")
 
@@ -214,23 +182,16 @@ full_pipeline = Pipeline([("preprocessor", preprocessor), ("estimators", estimat
 X_train, X_eval, y_train, y_eval = train_test_split(df1, df2, test_size=0.25, shuffle=True, stratify=df2,
                                                     random_state=RANDOM_SEED)
 
-# train model
+# train model with split data
 full_pipeline.fit(X_train, y_train)
 
 # predict on evaluation set
 eval_predict = full_pipeline.predict_proba(X_eval)
-
-predict_set = pd.DataFrame({"h1n1_vaccine": eval_predict[0][:, 1], "seasonal_vaccine": eval_predict[1][:, 1]},
-                           index=y_eval.index)
-predict_set.head()
-# print("Predict:", predict_set)
+eval_predict_set = pd.DataFrame({"h1n1_vaccine": eval_predict[0][:, 1], "seasonal_vaccine": eval_predict[1][:, 1]},
+                                index=y_eval.index)
 
 
-# AUC - ROC curve is a performance measurement for the classification problems at various threshold settings
-# AOC calculate the area under the ROC curve, which is abbreviated to AUC.
-# probability that a randomly chosen positive instance is ranked higher than a randomly chosen negative instance
-# ROC produced by calculating and plotting the true positive rate
-# against the false positive rate for a single classifier at a variety of thresholds
+# AUC - ROC curve
 def plot_roc(y_true, y_score, label_name, ax):
     fpr, tpr, thresholds = roc_curve(y_true, y_score)
     ax.plot(fpr, tpr)
@@ -239,21 +200,14 @@ def plot_roc(y_true, y_score, label_name, ax):
     ax.set_xlabel('FPR')
     ax.set_title(f"{label_name}: AUC = {roc_auc_score(y_true, y_score):.4f}")
 
-    fig, ax = plt.subplots(1, 2, figsize=(7, 3.5))
 
-    plot_roc(y_eval['h1n1_vaccine'], predict_set['h1n1_vaccine'], 'h1n1_vaccine', ax=ax[0])
-    plot_roc(y_eval['seasonal_vaccine'], predict_set['seasonal_vaccine'], 'seasonal_vaccine', ax=ax[1])
-    fig.tight_layout()
-    plt.show()
-
-
-# retain on full training set
+# retain model with full training set
 full_pipeline.fit(df1, df2)
 
 # load data from test set
 test_features_df = pd.read_csv("test_set_features.csv", index_col="respondent_id")
 
-# replacing values for test set
+# replace non numeric values for test set
 test_features_df['age_group'].replace(['18 - 34 Years', '35 - 44 Years', '45 - 54 Years', '55 - 64 Years', '65+ Years'],
                                       [0, 1, 2, 3, 4], inplace=True)
 test_features_df['education'].replace(['< 12 Years', '12 Years', 'Some College', 'College Graduate'], [0, 1, 2, 3],
@@ -268,8 +222,10 @@ test_features_df['employment_status'].replace(['Not in Labor Force', 'Unemployed
 test_features_df['census_msa'].replace(['Non-MSA', 'MSA, Not Principle  City', 'MSA, Principle City'], [0, 1, 2],
                                        inplace=True)
 
+# predict on test set
 test_predict = full_pipeline.predict_proba(test_features_df)
-
+# set dataframe to round the prediction
 test_predict_set = pd.DataFrame({"h1n1_vaccine": test_predict[0][:, 1], "seasonal_vaccine": test_predict[1][:, 1]},
                                 index=test_features_df.index)
-# print("Final:", test_predict_set)
+test_predict_set["h1n1_vaccine"] = test_predict_set["h1n1_vaccine"].round(0)
+test_predict_set["seasonal_vaccine"] = test_predict_set["seasonal_vaccine"].round(0)
